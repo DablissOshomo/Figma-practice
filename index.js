0225460f7b1fa1c66e1a76aaa9a1f1b3ddcfbc68
Andrew <AdinnuA5@gmail.com>
2023-08-10 20:39:55 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const navbar = document.querySelector('nav');
+ window.onscroll = () => { 
+    if (window.scrollY > 100) {
+         navbar.classList.add('nav-active');
+         } else { 
+            navbar.classList.remove('nav-active'); } };
